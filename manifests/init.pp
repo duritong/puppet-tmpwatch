9cee3ef2316f519377277b2fa00f3a361b821bcd
mh <mh@immerda.ch>
2009-01-06 22:33:18 +0000

    added tmpwatch module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,15 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# manifests/init.pp - manage tmpwatch stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "skeleton": }
-
-class skeleton {
+class tmpwatch {
     case $operatingsystem {
-        gentoo: { include skeleton::gentoo }
-        default: { include skeleton::base }
+        default: { include tmpwatch::base }
     }
 }
 
-class skeleton::base {
-    package{'skeleton':
+class tmpwatch::base {
+    package{'tmpwatch':
         ensure => installed,
     }
-
-    service{skeleton:
-        ensure => running,
-        enable => true,
-        #hasstatus => true, #fixme!
-        require => Package[skeleton],
-    }
-
-}
-
-class skeleton::gentoo inherits skeleton::base {
-    Package[skeleton]{
-        category => 'some-category',
-    }
-
-    #conf.d file if needed
-    # needs module gentoo
-    #gentoo::etcconfd { skeleton: require => "Package[skeleton]", notify => "Service[skeleton]"}
 }
